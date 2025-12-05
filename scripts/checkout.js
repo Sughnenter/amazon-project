95f39a1ac9b2438145dde857088846ebad920bb7
shammah <shamaj2006@gmail.com>
2025-12-05 16:33:47 +0100

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummary } from './checkout/orderSummary.js'
 import { rednerPaymentSummary } from './checkout/paymentSummary.js';
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 renderOrderSummary();
 rednerPaymentSummary();
